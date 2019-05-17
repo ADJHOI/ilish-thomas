9654764abab453b9de8cc716f6e834559248a375
Alfredahoy <Alfred.dj.hoi@gmail.com>
2019-05-17 21:38:10 +1200

    0.1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,21 @@
 $("#info").click(function(event){
     $("#descript").toggleClass("active");
 });
+
+$(document).scroll(function(){
+    var y = $(document);
+    if (y.scrollTop() > (window.innerHeight - 63)/4) {
+        $("#info").addClass("active");
+    } else {
+        $("#info").removeClass("active");
+        $("#descript").removeClass("active");
+    }
+    
+});
+
+$("#scroll").click(function(event){
+   $([document.documentElement, document.body]).animate({
+        scrollTop: $("#video").offset().top
+    }, 600); 
+});
+
