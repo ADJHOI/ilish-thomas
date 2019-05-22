9ffffe0c4852220ad49be55e70f6e4c39143160a
Alfredahoy <Alfred.dj.hoi@gmail.com>
2019-05-22 14:28:46 +1200

    0.1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,5 @@
-$("#info").click(function(event){
-    $("#descript").toggleClass("active");
-    if ($("#descript").hasClass("active")){
-        $("#sign").html("×")
-    } else {
-        $("#sign").html("+")
-    }
-});
-
-$(document).scroll(function(){
-    var y = $(document);
-    if (y.scrollTop() > (window.innerHeight - 63)/4) {
-        $("#info").addClass("active");
-    } else {
-        $("#info").removeClass("active");
-        $("#descript").removeClass("active");
-        $("#sign").html("+")
-    }  
-});
-
 $("#scroll").click(function(event){
    $([document.documentElement, document.body]).animate({
         scrollTop: $("#video").offset().top
     }, 600); 
 });
-
