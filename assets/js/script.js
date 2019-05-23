3f0bef50a336c1cf96b7ef9033a9726800d5246c
Alfredahoy <Alfred.dj.hoi@gmail.com>
2019-05-23 22:20:31 +1200

    0.1

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,10 +5,45 @@ $("#scroll").click(function(event){
 });
 
 $(document).scroll(function(event){
-//   console.log($(document).scrollTop());
     if($(document).scrollTop() > 20) {
         $(".footer").addClass("active");
     } else {
         $(".footer").removeClass("active");
     }
 });
+
+$("#counter-1").hover(function(event){
+    $("#citation-1").toggleClass("active");
+    $("#one").toggleClass("active");
+  }, function(event){
+    $("#citation-1").toggleClass("active");
+    $("#one").toggleClass("active");
+  }
+);
+
+$("#counter-2").hover(function(event){
+    $("#citation-2").toggleClass("active");
+    $("#two").toggleClass("active");
+  }, function(event){
+    $("#citation-2").toggleClass("active");
+    $("#two").toggleClass("active");
+  }
+);
+
+$("#counter-3").hover(function(event){
+    $("#citation-3").toggleClass("active");
+    $("#three").toggleClass("active");
+  }, function(event){
+    $("#citation-3").toggleClass("active");
+    $("#three").toggleClass("active");
+  }
+);
+
+$("#counter-4").hover(function(event){
+    $("#citation-4").toggleClass("active");
+    $("#four").toggleClass("active");
+  }, function(event){
+    $("#citation-4").toggleClass("active");
+    $("#four").toggleClass("active");
+  }
+);
