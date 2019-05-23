84e2b56ca1c383bb716b5970b2ebc67761413b00
Alfredahoy <Alfred.dj.hoi@gmail.com>
2019-05-23 19:37:17 +1200

    0.1

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,12 @@ $("#scroll").click(function(event){
         scrollTop: $("#video").offset().top
     }, 600); 
 });
+
+$(document).scroll(function(event){
+//   console.log($(document).scrollTop());
+    if($(document).scrollTop() > 20) {
+        $(".footer").addClass("active");
+    } else {
+        $(".footer").removeClass("active");
+    }
+});
