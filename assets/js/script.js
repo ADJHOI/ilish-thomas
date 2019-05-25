7c2bf18e08af4b6c97ecb05fe1eecef03508ae9c
Alfredahoy <Alfred.dj.hoi@gmail.com>
2019-05-25 16:43:17 +1200

    0.1

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,38 +12,108 @@ $(document).scroll(function(event){
     }
 });
 
-$("#counter-1").hover(function(event){
-    $("#citation-1").toggleClass("active");
-    $("#one").toggleClass("active");
-  }, function(event){
-    $("#citation-1").toggleClass("active");
-    $("#one").toggleClass("active");
-  }
-);
 
+$("#counter-1").hover(function(event){
+    if ($(window).width() > 699){
+        $("#citation-1").addClass("active");
+        $("#one").addClass("active");
+    }
+}, function(event){
+    if ($(window).width() > 699){
+        $("#citation-1").removeClass("active");
+        $("#one").removeClass("active");
+    }
+});
+
+$("#counter-1").click(function(event){
+    if ($(window).width() < 700){
+        $("#citation-1").toggleClass("active");
+        $("#mobile-1").toggleClass("active");  
+    }
+});
+            
 $("#counter-2").hover(function(event){
-    $("#citation-2").toggleClass("active");
-    $("#two").toggleClass("active");
-  }, function(event){
-    $("#citation-2").toggleClass("active");
-    $("#two").toggleClass("active");
-  }
-);
+    if ($(window).width() > 699){
+        $("#citation-2").addClass("active");
+        $("#two").addClass("active");
+    }
+}, function(event){
+    if ($(window).width() > 699){
+        $("#citation-2").removeClass("active");
+        $("#two").removeClass("active");
+    }
+});
+
+$("#counter-2").click(function(event){
+    if ($(window).width() < 700){
+        $("#citation-2").toggleClass("active");
+        $("#mobile-2").toggleClass("active");  
+    }
+});
 
 $("#counter-3").hover(function(event){
-    $("#citation-3").toggleClass("active");
-    $("#three").toggleClass("active");
-  }, function(event){
-    $("#citation-3").toggleClass("active");
-    $("#three").toggleClass("active");
-  }
-);
+    if ($(window).width() > 699){
+        $("#citation-3").addClass("active");
+        $("#three").addClass("active");
+    }
+}, function(event){
+    if ($(window).width() > 699){
+        $("#citation-3").removeClass("active");
+        $("#three").removeClass("active");
+    }
+});
+
+$("#counter-3").click(function(event){
+    if ($(window).width() < 700){
+        $("#citation-3").toggleClass("active");
+        $("#mobile-3").toggleClass("active");  
+    }
+});
 
 $("#counter-4").hover(function(event){
-    $("#citation-4").toggleClass("active");
-    $("#four").toggleClass("active");
-  }, function(event){
-    $("#citation-4").toggleClass("active");
-    $("#four").toggleClass("active");
-  }
-);
+    if ($(window).width() > 699){
+        $("#citation-4").addClass("active");
+        $("#four").addClass("active");
+    }
+}, function(event){
+    if ($(window).width() > 699){
+        $("#citation-4").removeClass("active");
+        $("#four").removeClass("active");
+    }
+});
+
+$("#counter-4").click(function(event){
+    if ($(window).width() < 700){
+        $("#citation-4").toggleClass("active");
+        $("#mobile-4").toggleClass("active");  
+    }
+});
+
+$(".close").click(function(event){
+    $(this).parent().removeClass("active");
+    $(this).parent().prev().children().removeClass("active")
+});
+            
+$(function () {
+    var touch = 'ontouchstart' in document.documentElement
+                || navigator.maxTouchPoints > 0
+                || navigator.msMaxTouchPoints > 0;
+
+    if (touch) {
+        try {
+
+            for (var si in document.styleSheets) {
+                var styleSheet = document.styleSheets[si];
+                if (!styleSheet.rules) continue;
+
+                for (var ri = styleSheet.rules.length - 1; ri >= 0; ri--) {
+                    if (!styleSheet.rules[ri].selectorText) continue;
+
+                    if (styleSheet.rules[ri].selectorText.match(':hover')) {
+                        styleSheet.deleteRule(ri);
+                    }
+                }
+            }
+        } catch (ex) {}
+    }
+});   
